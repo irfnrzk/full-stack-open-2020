121bdcf88da54c3cb8ac0b4d4c79f7bde73a103f
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-10-29 13:59:31 +0800

    3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[5]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.sqszd.mongodb.net/persons?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+// console.log(process.argv.length)
+
+if (process.argv.length > 3) {
+  const person = new Person({
+    "name": name || "Arto Hellas",
+    "number": number || "040-123456"
+  })
+
+  person.save().then(result => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person
+    .find({})
+    .then(persons => {
+      console.log('phonebook:');
+      persons.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+}
