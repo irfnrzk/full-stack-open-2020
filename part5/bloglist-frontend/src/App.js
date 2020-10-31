c52d58d13feeba3fb1292598e1df6193ccbf2dd0
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-10-31 21:32:38 +0800

    5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -8,6 +8,11 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlog, setNewBlog] = useState({
+    title: null,
+    author: null,
+    url: null
+  })
   // const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
@@ -57,6 +62,16 @@ const App = () => {
     setUser(null)
   }
 
+  const handlePost = (event) => {
+    event.preventDefault()
+    // console.log(newBlog)
+    blogService
+      .create(newBlog)
+      .then(blog =>
+        setBlogs(blogs.concat(blog))
+      )
+  }
+
   const loginForm = () => (
     <>
       <h1>login in to application</h1>
@@ -93,6 +108,36 @@ const App = () => {
         >logout</button>
       </div>
       <br />
+      <div>
+        <h1>create new</h1>
+        <form onSubmit={handlePost}>
+          <div>
+            title
+            <input
+              type="text"
+              name="title"
+              onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })}
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="author"
+              name="author"
+              onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })}
+            />
+          </div>
+          <div>
+            url
+            <input
+              type="url"
+              name="url"
+              onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
       <div>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
