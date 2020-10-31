ef8c52b3f56325bff46d782c5a0cc7c2b6ad9a6a
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-10-31 13:33:11 +0800

    4.15

---
+++ part4/bloglist/tests/user_api.test.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const User = require('../models/user')
+const api = supertest(app)
+const bcrypt = require('bcrypt')
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await api.get('/api/users')
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await api.get('/api/users')
+    expect(usersAtEnd.body).toHaveLength(usersAtStart.body.length + 1)
+    const usernames = usersAtEnd.body.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await api.get('/api/users')
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await api.get('/api/users')
+    expect(usersAtEnd.body).toHaveLength(usersAtStart.body.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
