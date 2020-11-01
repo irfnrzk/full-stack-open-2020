6753acfaa287945ef3f99b13893dbf0d0ceea859
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-11-01 23:30:08 +0800

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
@@ -35,6 +35,42 @@ test(`checks Blog component by default renders title & author without url & numb
   )
 
   expect(component.container.querySelector('div + div')).toHaveClass(
+    'd-none'
+  )
+})
+
+test(`checks blog's url & likes shows when view button is clicked`, () => {
+  const blog = {
+    author: 'Michael Chan',
+    id: '5f9d2abb314d084db48decf5',
+    likes: 20,
+    title: 'React patterns',
+    url: 'https://reactpatterns.com/',
+    user: {
+      id: '5f9d02cad213823b143d5049',
+      name: 'Michael Chan',
+      username: 'Michael'
+    }
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Blog
+      key={blog.id}
+      blog={blog}
+      username={'Michael'}
+      toggleView={mockHandler}
+    />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  // const div = component.container.querySelector('div')
+  // console.log(prettyDOM(div))
+
+  expect(component.container.querySelector('div + div')).not.toHaveClass(
     'd-none'
   )
 })
