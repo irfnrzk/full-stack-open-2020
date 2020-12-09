680b1296b838c70c883800aa85e5f826f39ed351
Muhammad Irfan Bin Ab Razak <irfan.abrazak@gmail.com>
2020-12-09 20:03:59 +0800

    7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,5 +1,5 @@
-  
-import React, { useState, useEffect } from 'react'
+
+import React, { useState } from 'react'
 import axios from 'axios'
 
 
@@ -20,14 +20,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then((response) => setResources(response.data))
+  }
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
   }
 
   const service = {
-    create
+    create,
+    getAll
   }
 
   return [
@@ -47,10 +52,10 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -64,7 +69,7 @@ const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
