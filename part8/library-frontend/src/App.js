5f27010f23c39c9c2ac0a4f8892f81eb3c2b2a2d
Muhammad Irfan Bin Ab Razak <irfan.abrazak@gmail.com>
2020-12-25 21:14:59 +0800

    8.8

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -3,9 +3,25 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors{
+    name,
+    born,
+    bookCount
+  }
+}
+`
 
 const App = () => {
+  const result = useQuery(ALL_AUTHORS)
   const [page, setPage] = useState('authors')
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -17,6 +33,7 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        authors={result.data.allAuthors}
       />
 
       <Books
